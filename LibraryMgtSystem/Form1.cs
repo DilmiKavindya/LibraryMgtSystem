501ca69fc6f9187c7f28f09cdca0af5e5929f36a
Dilmi Kavindya <kavindyadilmi00@gmail.com>
2025-07-28 01:03:45 +0530

    Created Login Form

--- LibraryMgtSystem/Form1.cs
+++ LibraryMgtSystem/Form1.cs
@@ -6,5 +6,42 @@ namespace LibraryMgtSystem
         {
             InitializeComponent();
         }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUsername_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsername_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (txtUsername.Text == "Username")
+            {
+                txtUsername.Clear();
+            }
+        }
+
+        private void txtPassword_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (txtPassword.Text == "Password")
+            {
+                txtPassword.Clear();
+                txtPassword.PasswordChar = '*';
+            }
+        }
+
+        private void pictureBoxYoutube_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("");
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
