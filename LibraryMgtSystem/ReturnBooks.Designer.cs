442792a9c82f1aabf220ed89ac0b4f443988545f
DilmiKavindya <kavindyadilmi00@gmail.com>
2025-09-12 21:44:30 +0530

    Designed the Return Book page

---
+++ LibraryMgtSystem/ReturnBooks.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace LibraryMgtSystem
+{
+    partial class ReturnBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReturnBooks));
+            panel1 = new Panel();
+            dataGridView1 = new DataGridView();
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Silver;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(2, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(302, 568);
+            panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(305, 2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(701, 421);
+            dataGridView1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DarkGray;
+            panel2.Location = new Point(305, 425);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(702, 145);
+            panel2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(95, 18);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(91, 86);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 9F);
+            label1.Location = new Point(68, 130);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 17);
+            label1.TabIndex = 1;
+            label1.Text = "Enter Enrollment No.";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(55, 163);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(168, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(95, 236);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // ReturnBooks
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            BackColor = Color.FromArgb(255, 192, 128);
+            ClientSize = new Size(1006, 446);
+            Controls.Add(panel2);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel1);
+            Name = "ReturnBooks";
+            Text = "ReturnBooks";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private DataGridView dataGridView1;
+        private Panel panel2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
